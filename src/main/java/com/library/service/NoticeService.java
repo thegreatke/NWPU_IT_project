558636e7c2b7eecbc8f2018232e7941a1622acfc
JackLong <478613685@qq.com>
2019-09-25 16:05:19 +0800

    ljg
    1.0

--- src/main/java/com/library/service/NoticeService.java
+++ src/main/java/com/library/service/NoticeService.java
@@ -1,8 +1,29 @@
 package com.library.service;
 
+import com.library.bean.lib_notice;
+import com.library.dao.NoticeDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NoticeService {
 
     // TODO: 2019/9/23  实现管理公告版的操作，保存到数据库的List_notice的公告表中去，增删改查
+
+    @Autowired
+    private NoticeDao noticeDao;
+
+    public boolean addNotice(lib_notice notice) {
+        return noticeDao.addNotice(notice) > 0;
+    }//返回的int是数据库执行操作后影响的行数
+
+    public lib_notice getNotice(Long noticeId) {
+        return noticeDao.getNotice(noticeId);
+    }
+
+    public boolean deleteNotice(Long noticeId) {
+        return noticeDao.deleteNotice(noticeId) > 0;
+    }
 
 
 
