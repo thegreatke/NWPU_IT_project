ace0b861c6c10a5fd15cb3ccf021def717723d07
kezhou_mac <229400458>
2019-10-07 16:55:44 +0800

    kill_bug
    ..

--- src/main/java/com/library/controller/BookController.java
+++ src/main/java/com/library/controller/BookController.java
@@ -5,14 +5,11 @@ import com.library.bean.Lend;
 import com.library.bean.ReaderCard;
 import com.library.service.BookService;
 import com.library.service.LendService;
-import com.sun.org.apache.xpath.internal.operations.Mod;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
